6a8759223defeffd7980ff5a97e6d682f1365fd9
Kent Delos Santos <kent.delossantos1506@gmai;.com>
2026-07-10 02:19:27 +0800

    Initial Clean Architecture setup

---
+++ ExpenseTracker.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using ExpenseTracker.Domain.Common;
+
+namespace ExpenseTracker.Domain.Entities;
+
+public class Category : BaseEntity
+{
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Color { get; set; } = "#6366F1";
+
+    public string Icon { get; set; } = "category";
+
+    public List<Transaction> Transactions { get; set; } = new();
+}
---
+++ ExpenseTracker.Domain/Entities/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using ExpenseTracker.Domain.Common;
+using ExpenseTracker.Domain.Enums;
+
+namespace ExpenseTracker.Domain.Entities;
+
+public class Transaction : BaseEntity
+{
+
+    public TransactionType Type { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public Category? Category { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string Notes { get; set; } = string.Empty;
+
+    public DateTime Date { get; set; } = DateTime.Now;
+}
